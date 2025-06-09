da789ee8c6550d820ed0eb0ae7dbaab2d7bf3796
uwi0 <kakapoweb@gmail.com>
2025-06-10 03:21:00 +0700

    Initial Commit

---
+++ Simple Tca Navigation/Simple_Tca_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Tca_NavigationApp.swift
+//  Simple Tca Navigation
+//
+//  Created by dwi prasetyo on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Tca_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
